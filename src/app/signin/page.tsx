6b68df1677f1a54ea8dd29ac22c36b95f479d404
Glitch-guy0 <prajwalm9845@gmail.com>
2024-07-22 11:14:25 +0530

    signin page; white theme
    signin page for white theme and optimized only for mobile devices
    - working form with error display except signin button conditionals
    todo:
    - dark theme
    - for larger devices make width 95% and height 95% and landscape still
      working with design

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,45 @@
+export default function signinPage() {
+  return (
+    // sets background and centers the form page
+    <div className=" bg-slate-200 w-full h-full flex items-center justify-center">
+      {/* for styling the form page */}
+      <div className="bg-slate-100 shadow-lg p-3 rounded-lg max-w-[360px] w-2/3">
+        <h1 className="text-center font-bold text-2xl">Sign in</h1>
+
+        <form action="" className="flex flex-col gap-4  mt-4">
+          <input
+            type="text"
+            name="username"
+            id=""
+            placeholder="Username"
+            className="outline-none p-2 rounded-lg outline-2 focus:outline-blue-400" 
+          />
+          <input
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+            className="peer/email outline-none p-2 rounded-lg outline-2 focus:outline-blue-400 invalid:text-rose-400 invalid:outline-rose-400" 
+          />
+          {/* error for email */}
+          <p className="peer-invalid/email:block hidden text-rose-400 text-xs">*Invalid Email</p>
+          <input
+            type="password"
+            name="password"
+            id=""
+            // checks for min, max no. of chars
+            minLength={8}
+            maxLength={20}
+            placeholder="Password"
+            // accepts atleast 8 characters which containing atleast one lowercase,uppercase and number
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$"
+            className="peer/password outline-none p-2 rounded-lg outline-2 focus:outline-blue-400 invalid:text-rose-400 invalid:outline-rose-400" 
+            />
+          {/* error for password */}
+          <p className="peer-invalid/password:block hidden text-rose-400 text-xs">*minimum 8 characters should contain atleast one lowcase, uppercase & number</p>
+          <input type="submit" value="Sign in" className="p-2 bg-cyan-200 font-semibold text-xl rounded-lg" />
+        </form>
+      </div>
+    </div>
+  );
+}
